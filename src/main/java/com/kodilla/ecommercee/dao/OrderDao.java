775134ebc5be6ee118d080ffd19ae72b32152f80
Makedo81 <domi.stan@wp.pl>
2019-05-12 18:50:52 +0200

    OrderDao methods implementation save(),findAll(),findById(),deleteById()

--- src/main/java/com/kodilla/ecommercee/dao/OrderDao.java
+++ src/main/java/com/kodilla/ecommercee/dao/OrderDao.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee.dao;
 
-public interface OrderDao {
+import com.kodilla.ecommercee.domain.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface OrderDao extends CrudRepository<Order,Long> {
+
+    @Override
+    Order save(Order order);
+
+    @Override
+    void deleteById(Long Id);
+
+    @Override
+    List<Order> findAll();
+
+    @Override
+    Optional<Order> findById(Long Id);
 }
