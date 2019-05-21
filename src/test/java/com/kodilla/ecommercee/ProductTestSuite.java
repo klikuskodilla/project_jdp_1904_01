dad19bfa09a222680b62ec5c077415af1531722f
golfik2005 <spiotrek1985@gmail.com>
2019-05-21 20:55:55 +0200

    - dodanie relationshipTests() dla relacji z innymi encjami

--- src/test/java/com/kodilla/ecommercee/ProductTestSuite.java
+++ src/test/java/com/kodilla/ecommercee/ProductTestSuite.java
@@ -1,7 +1,10 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.dao.CartDao;
+import com.kodilla.ecommercee.dao.OrderDao;
 import com.kodilla.ecommercee.dao.ProductDao;
-import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.dao.ProductGroupDao;
+import com.kodilla.ecommercee.domain.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,15 @@ import static org.junit.Assert.assertEquals;
 public class ProductTestSuite {
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private ProductGroupDao productGroupDao;
+
+    @Autowired
+    private CartDao cartDao;
+
+    @Autowired
+    private OrderDao orderDao;
 
     @Test
     public void saveAndFindByIdTest(){
@@ -32,5 +44,43 @@ public class ProductTestSuite {
 
         //Clean Up
         productDao.deleteById(id);
+    }
+
+    @Test
+    public void relationshipTests(){
+        //Given
+        Product product = new Product("Shoes", 100);
+        ProductGroup productGroup = new ProductGroup("Sport shoes");
+        Cart cart = new Cart();
+        Order order = new Order("OPEN");
+
+        product.setOrder(order);
+        product.setCart(cart);
+        product.setProductGroup(productGroup);
+
+        productGroup.getProducts().add(product);
+        cart.getProductList().add(product);
+        order.getOrderedProducts().add(product);
+        order.setCart(cart);
+
+        //When
+        productDao.save(product);
+        cartDao.save(cart);
+        orderDao.save(order);
+        productGroupDao.save(productGroup);
+        Product productTest = productDao.findById(product.getId()).get();
+
+        //Then
+        assertEquals("Shoes", productTest.getName());
+        assertEquals(100, productTest.getPrize(), 0.01);
+        assertEquals("Sport shoes", productTest.getProductGroup().getDescription());
+        assertEquals(cart.getId(), productTest.getCart().getId());
+        assertEquals("OPEN", productTest.getOrder().getStatus());
+
+        //Clean Up
+        orderDao.deleteById(order.getId());
+        cartDao.deleteById(cart.getId());
+        productGroupDao.deleteById(productGroup.getId());
+        productDao.deleteById(product.getId());
     }
 }
