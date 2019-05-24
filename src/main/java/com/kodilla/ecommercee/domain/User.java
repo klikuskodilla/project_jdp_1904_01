f96faec86d85658ecd424a61ad3c67638cc7b3f4
Makedo81 <domi.stan@wp.pl>
2019-05-24 22:34:06 +0200

    usuniecie zbednych importow z encji User

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -4,14 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 @AllArgsConstructor
 @NoArgsConstructor
